ec9c3dd97d3cdc63a79a9c2b4830ee289d0668f3
Richard Balint <richardbalint@proton.me>
2023-11-14 11:41:23 +0100

    update

--- src/components/Gridbox.js
+++ src/components/Gridbox.js
@@ -5,34 +5,38 @@ import Singapor from "../images/singapore.jpg"
 
 export default function Gridbox() {
   return (
-    <div className='cards'>
-      <div className='card card1'>
-        <div className='container'>
-          <img src={Vegas} alt="" />
+    <div className="cards">
+
+      <div className="card card1">
+        <div className="container">
+            <img src={Vegas} alt="las vegas"/>
         </div>
-        <div className='details'>
-          <h3>Las Vegas</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus aperiam adipisci exercitationem.</p>
-        </div>
+        <div className="details">
+            <h3>Las Vegas</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus aperiam adipisci exercitationem.</p>
       </div>
-      <div className='card card2'>
-        <div className='container'>
-          <img src={Newyork} alt="" />
+    </div>
+
+    <div className="card card2">
+        <div className="container">
+            <img src={Newyork} alt="New York"/>
         </div>
-        <div className='details'>
+        <div className="details">
             <h3>New York</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus aperiam adipisci exercitationem.</p>
         </div>
-      <div className='card card3'>
-        <div className='container'>
-          <img src={Singapor} alt="" />
-        </div>
-        <div className='details'>
-          <h3>Singapor</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus aperiam adipisci exercitationem.</p>
-        </div>
-      </div>
-      </div>
     </div>
+
+    <div className="card card3">
+        <div className="container">
+            <img src={Singapor} alt="Singapore"/>
+        </div>
+        <div className="details">
+            <h3>Singapore</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus aperiam adipisci exercitationem.</p>
+        </div>
+    </div>
+
+</div>
   )
 }
